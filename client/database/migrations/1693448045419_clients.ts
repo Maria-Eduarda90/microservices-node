ef2f358f07f19060c1db5cb4f49b8705489c6b5d
Mary Skywalker <snowdennaruto90@gmail.com>
2023-08-31 02:15:19 -0400

    creation of model and migration client

---
+++ client/database/migrations/1693448045419_clients.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'clients'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+      table.string('name', 255).notNullable()
+      table.string('email', 255).notNullable().unique()
+      table.string('password', 180).notNullable()
+      table.string('phone', 11).notNullable()
+      
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
