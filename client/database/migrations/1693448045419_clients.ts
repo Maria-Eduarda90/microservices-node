1084c4e62826dfcef77866d326e9037d1bdf996f
Mary Skywalker <snowdennaruto90@gmail.com>
2023-09-01 02:12:47 -0400

    uuid field replaced by increment field

--- client/database/migrations/1693448045419_clients.ts
+++ client/database/migrations/1693448045419_clients.ts
@@ -5,11 +5,11 @@ export default class extends BaseSchema {
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.uuid('id').primary()
+      table.increments('id').primary()
       table.string('name', 255).notNullable()
       table.string('email', 255).notNullable().unique()
       table.string('password', 180).notNullable()
-      table.string('phone', 11).notNullable()
+      table.string('phone', 180).notNullable()
       
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
