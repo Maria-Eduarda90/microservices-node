f0d339850ee5a4b28a020184daf39194d657efc1
meyh <meyh@gmail.com>
2023-10-13 18:29:46 -0300

    logic

---
+++ client/src/modules/@types/ClientUseCaseType.ts
@@ -0,0 +1,6 @@
+type ClientUseCaseType = {
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+}
---
+++ client/src/modules/usecase/client-client.usecase.ts
@@ -0,0 +1,7 @@
+export class CreateClientUseCase {
+  constructor() {}
+
+  async execute(data: ClientUseCaseType) {
+    
+  }
+}
