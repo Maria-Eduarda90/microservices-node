7539f5f3b7eed3994f1eac75d3f19ea3a2fcb2b3
meyh <meyh@gmail.com>
2023-10-15 06:05:58 -0300

    created customer

--- requests/src/providers/kafka/consumers/create-customer.consumer.ts
+++ requests/src/providers/kafka/consumers/create-customer.consumer.ts
@@ -1,12 +1,21 @@
+import { prismaClient } from "../../../config/prisma/prismaClient";
 import { kafkaConsumer } from "../kafka.consumer";
+import { CustomerConsumerType } from "./@types/CustomerConsumerType";
 
 export async function  createCustomerConsumer(){
     console.log('CUSTOMER CONSUMER');
     const consumer = await kafkaConsumer('CUSTOMER_CREATED');
     await consumer.run({
         eachMessage: async ({ message }) => {
-            const messageToString = message.value?.toString();
-            console.log("messageToString: ", messageToString);
+            const messageToString = message.value!.toString();
+            const customer = JSON.parse(messageToString) as CustomerConsumerType;
+            
+            await prismaClient.customer.create({
+                data: {
+                    externalId: customer.id,
+                    email: customer.email,
+                }
+            });
         }
     });
 }
