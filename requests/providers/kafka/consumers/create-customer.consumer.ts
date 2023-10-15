26eaff8f866d1479f8cd38a19ff4b95a06d7847b
meyh <meyh@gmail.com>
2023-10-15 03:47:42 -0300

    created consumer

---
+++ requests/providers/kafka/consumers/create-customer.consumer.ts
@@ -0,0 +1,11 @@
+import { kafkaConsumer } from "../kafka.consumer";
+
+export async function  createCustomerConsumer(){
+    const consumer = await kafkaConsumer('CUSTOMER_CREATED');
+    await consumer.run({
+        eachMessage: async ({ message }) => {
+            const messageToString = message.value?.toString();
+            console.log(messageToString);
+        }
+    })
+}
---
+++ requests/providers/kafka/index.ts
@@ -0,0 +1,13 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  brokers: ["humble-seal-6082-us1-kafka.upstash.io:9092"],
+  sasl: {
+    mechanism: "scram-sha-256",
+    username: `${process.env.USERNAME_KAFKA}`,
+    password: `${process.env.PASSWORD_KAFKA}`,
+  },
+  ssl: true,
+});
+
+export { kafka };
