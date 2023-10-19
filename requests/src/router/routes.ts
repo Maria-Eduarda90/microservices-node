2bc9b6471a085f5e94884f95e7e8aaf45e775181
meyh <meyh@gmail.com>
2023-10-19 17:42:01 -0300

    status update route created

--- requests/src/router/routes.ts
+++ requests/src/router/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { CreateOrderController } from "../modules/controllers/create-order.controller";
+import { UpdateOrderController } from "../modules/update/update-order.controller";
 
 const router = Router();
 
 router.post('/orders', (request, response) => {
     new CreateOrderController().handle(request, response);
-})
+});
+
+router.patch("/orders", (request, response) => {
+  new UpdateOrderController().handle(request, response);
+});
 
 export { router }
