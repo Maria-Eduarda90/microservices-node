47284c2c016aa52691065527350222355ad8e037
meyh <meyh@gmail.com>
2023-10-19 17:05:23 -0300

    order route added to the server

--- requests/src/server.ts
+++ requests/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import "./providers/kafka/consumers"
+import { router } from "./router/routes";
 
 const PORT = process.env.PORT ?? 3002;
 
 const app = express();
 app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => console.log(`Server client is running on PORT ${PORT}`));
